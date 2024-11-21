8227612832ffe2f739c2a6569227771a97058599
hacimertgokhan <gokhan.hacimert@ogr.ahievran.edu.tr>
2024-11-21 10:20:00 +0300

    Başlatma kodu değiştirildi ve zip & tar desteği direkt olarak maven'a eklendi.

--- ddb.sh
+++ ddb.sh
@@ -1,2 +1,3 @@
-echo "DenisDB"
-java -Dfile.encoding=UTF-8 -jar DenisDB-[version]-jwd.jar
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Denis $VERSION, All rights reserved."
+java -Dfile.encoding=UTF-8 -jar DenisDB-$VERSION-jwd.jar

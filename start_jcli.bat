2b75835bf4f483753dfd3ad2a92a7c1a54c82438
hacimertgokhan <gokhan.hacimert@ogr.ahievran.edu.tr>
2024-12-05 21:06:39 +0300

    Genel yeniden yapılandırma.

--- start_jcli.bat
+++ start_jcli.bat
@@ -1,4 +1,4 @@
 @echo off
-set VERSION=0.0.0.4
-echo Denis Database Integrated CLI (0.0.1alpha)
+set VERSION=0.0.2.5
+echo Denis Database Integrated CLI (jcli-win-0.0.1alpha)
 java -Dfile.encoding=UTF-8 -cp "denis-%VERSION%-alpha.jar;lib/*" github.hacimertgokhan.denisdb.cli.CLIMain
--- start_jcli.sh
+++ start_jcli.sh
@@ -1,3 +1,3 @@
-VERSION=0.0.0.4
-echo "Denis Database Integrated CLI (0.0.2alpha)"
+VERSION=0.0.2.5
+echo "Denis Database Integrated CLI (jcli-lnx-0.0.2alpha)"
 java -Dfile.encoding=UTF-8 -cp "denis-$VERSION-alpha.jar:lib/*" github.hacimertgokhan.denisdb.cli.CLIMain

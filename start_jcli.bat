29dbc29cc07f6416dc8d9596be045123db2ea5b0
hacimertgokhan <gokhan.hacimert@ogr.ahievran.edu.tr>
2024-12-07 18:44:56 +0300

    Shell scriptleri güncellendi.

--- start_jcli.bat
+++ start_jcli.bat
@@ -1,4 +1,9 @@
 @echo off
-set VERSION=0.0.2.7
+setlocal enabledelayedexpansion
+for /f "tokens=1,2 delims==" %%A in (denis.conf) do (
+    if "%%A"=="sem_ver" (
+        set VERSION=%%B
+    )
+)
 echo Denis Database Integrated CLI (jcli-win-0.0.1alpha)
 java -Dfile.encoding=UTF-8 -cp "denis-%VERSION%-alpha.jar;lib/*" github.hacimertgokhan.denisdb.cli.CLIMain
--- start_jcli.sh
+++ start_jcli.sh
@@ -1,3 +1,3 @@
-VERSION=0.0.2.7
+VERSION=$(grep -oP '^sem_ver=\K.*' denis.conf)
 echo "Denis Database Integrated CLI (jcli-lnx-0.0.2alpha)"
 java -Dfile.encoding=UTF-8 -cp "denis-$VERSION-alpha.jar:lib/*" github.hacimertgokhan.denisdb.cli.CLIMain
